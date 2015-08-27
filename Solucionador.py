0db4ac45b9c9090502c5b05b61617d0d6143fbeb
Matheus Mayron <matheusmayron@gmail.com>
2015-08-27 20:11:52 -0300

    Ate agora, nada demais

--- Solucionador.py
+++ Solucionador.py
@@ -1,1 +1,9 @@
 __author__ = 'Matheus'
+from No import *
+
+class Solucionador:
+    def __init__(self, tabuleiroInicial):
+        self.raizDaArvoreDePossibilidades = No(None,None,tabuleiroInicial)
+
+
+
