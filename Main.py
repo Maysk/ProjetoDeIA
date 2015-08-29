46f4fd6aee92359c0392bb92c1c5fe1260c287b9
LucasFalcao <lucasfalcao.p@hotmail.com>
2015-08-28 23:13:26 -0300

    Consertado código quebrado inserido pelo Matheus

--- Main.py
+++ Main.py
@@ -1,20 +1,17 @@
 from PyQt4.QtGui import *
-from PyQt4.QtCore import
+from PyQt4.QtCore import *
 from Solucionador import *
 from EstruturaDeArvore import *
 from Desenho import *
 import sys
 
 
-def main():
-	
+def main():	
     #app = QApplication(sys.argv)
     
     listaNumeros = [1,2,3,4,5,6,7,0,8]
 
     s = Solucionador(listaNumeros)
-
-
 
 
     #a = Desenho(listaPecas=listaNumeros)
@@ -24,8 +21,7 @@ def main():
     #listaMovimentos = ['w','w','a','a','a','d','s']
     #a.getListaMovimentos(listaMovimentos)
     
+    #return app.exec_()
 
-	
 if __name__ == "__main__":
-	main()
-	
+    main()
